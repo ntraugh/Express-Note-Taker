73b7f063374a4daa863ae728fd0cf1a5e8574496
Nate Traugh <n.traugh@yahoo.com>
2022-06-20 08:28:05 -0400

    added app.get for the notes page, notes page now shows when you click get started

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require("express")
 const path = require("path")
 const PORT = 3001; 
+const app = express();
+
+app.use(express.static("public"))
+
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+)
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+)
