b472d9fdf20eb23ff791806cd587061bc117a721
Nate Traugh <n.traugh@yahoo.com>
2022-06-20 09:23:45 -0400

    added app.get for our notes section

--- server.js
+++ server.js
@@ -4,27 +4,29 @@ const PORT = 3001;
 const app = express();
 // importing our db.json file
 const everyNote = require("./db/db.json")
-console.log(everyNote)
+
 
 app.use(express.static("public"))
 
 // this is just "/" because it is our main page
-// app.get("/", (req, res) =>
-//     res.sendFile(path.join(__dirname, "public/index.html"))
-// )
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
+)
 
-// app.get("/notes", (req, res) =>
-//     res.sendFile(path.join(__dirname, "public/notes.html"))
-// )
-
-
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+)
 
 
+app.get("/api/notes", (req, res) => {
+    res.json(everyNote)
+})
 
 
 
 
 
-// app.listen(PORT, () =>
-//     console.log(`Example app listening at http://localhost:${PORT}`)
-// )
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+)
