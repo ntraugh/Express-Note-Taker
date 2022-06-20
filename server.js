9a8eb5d899b02546a7a0efadfbb5972150245144
Nate Traugh <n.traugh@yahoo.com>
2022-06-20 09:18:07 -0400

    imported db.json to server.js, console logged everyNote, getting back an array of objects

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require("express")
 const path = require("path")
 const PORT = 3001; 
 const app = express();
+// importing our db.json file
+const everyNote = require("./db/db.json")
+console.log(everyNote)
 
 app.use(express.static("public"))
 
-app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-)
+// this is just "/" because it is our main page
+// app.get("/", (req, res) =>
+//     res.sendFile(path.join(__dirname, "public/index.html"))
+// )
+
+// app.get("/notes", (req, res) =>
+//     res.sendFile(path.join(__dirname, "public/notes.html"))
+// )
 
 
 
@@ -17,6 +25,6 @@ app.get("/notes", (req, res) =>
 
 
 
-app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
-)
+// app.listen(PORT, () =>
+//     console.log(`Example app listening at http://localhost:${PORT}`)
+// )
