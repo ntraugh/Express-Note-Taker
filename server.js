b23c576d54039a982455c9bdb384c267f21cb632
Nate Traugh <n.traugh@yahoo.com>
2022-06-21 16:10:15 -0400

    commented out the old function, added the uuid to helper folder to give each note a unique id, still need to get the note to repopulate once clicked on

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const PORT = 3001;
 const app = express();
 // importing our db.json file
 const everyNote = require("./db/db.json")
-const fs = require ("fs")
+const fs = require ("fs");
+const uuid = require('./helpers/uuid');
+const { json } = require("express");
+
 
 // accept form part data, accept json, and allow pages to show 
 app.use(express.urlencoded({ extended: true}))
@@ -23,19 +26,45 @@ app.get("/notes", (req, res) =>
 
 app.get("/api/notes", (req, res) => res.json(everyNote));
 
-// function to save the note to the left side of the page
-function saveNewNote (body, allNotes){
-    const noteBody = body
-    allNotes.push(noteBody)
-    fs.writeFileSync(path.join(__dirname, "./db/db.json"),
-        JSON.stringify(allNotes, null, 2));
-        return noteBody
-}
+// // function to save the note to the left side of the page
+// function saveNewNote (body, allNotes){
+//     const noteBody = body
+//     allNotes.push(noteBody)
+//     fs.writeFileSync(path.join(__dirname, "./db/db.json"),
+//         JSON.stringify(allNotes, null, 2));
+//         return noteBody
+// }
 
 app.post("/api/notes", (req, res) => {
-    const makeNewNote = saveNewNote(req.body, everyNote)
-    console.log(makeNewNote)
-    res.json(makeNewNote)
+    // const makeNewNote = saveNewNote(req.body, everyNote)
+    // console.log(makeNewNote)
+    // res.json(makeNewNote)
+    console.info(`${req.method} request received to add note`)
+    const { title, text } = req.body
+    if(title && text){
+        const newNote = {
+            title, 
+            text,
+            note_id: uuid()
+        }
+        const noteData = fs.readFileSync("./db/db.json", "utf-8");
+        const notes = noteData.length ? JSON.parse(noteData) : [];
+        notes.push(newNote)
+        // convert data to a string to save it
+        const noteString = JSON.stringify(notes, null, 2)
+        fs.writeFile(`./db/db.json`, noteString, (err) => 
+            err
+                ? console.error(err)
+                : console.log(
+                    `A new note with the text of ${newNote.text} has been created`
+                    )
+        )
+        const response = {
+            status: "success",
+            body: newNote,
+        }
+        console.log(response)
+    }
 })
 
 app.delete("/api/notes", (req, res) => {
